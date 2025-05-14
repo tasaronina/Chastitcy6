9cac3c62a7b42c1f7336e40738d00edfccf8a0e1
kuromi75@yandex.ru <kuromi75@yandex.ru>
2025-05-14 10:44:07 +0800

    Добавьте файлы проекта.

---
+++ Chastitcy6/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chastitcy6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // привязал изображение
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+        }
+    }
+}
