e7b47fbb96f691c10bac94a80fb8b4661eef00c5
kuromi75@yandex.ru <kuromi75@yandex.ru>
2025-05-14 16:31:26 +0800

    создала эммитер

---
+++ Chastitcy6/Emitter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chastitcy6
+{
+    class Emitter
+    {
+        List<Particle> particles = new List<Particle>();
+        public int MousePositionX;
+        public int MousePositionY;
+
+        public void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1; // уменьшаю здоровье
+                                    // если здоровье кончилось
+                if (particle.Life < 0)
+                {
+                    // восстанавливаю здоровье
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    // новое начальное расположение частицы — это то, куда указывает курсор
+                    particle.X = MousePositionX;
+
+                    /* ЭТО ДОБАВЛЯЮ, тут сброс состояния частицы */
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+                    /* конец ЭТО ДОБАВЛЯЮ  */
+
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+            }
+
+            // добавил генерацию частиц
+            // генерирую не более 10 штук за тик
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
+                {
+                    // а у тут уже наш новый класс используем
+                    var particle = new ParticleColorful();
+                    // ну и цвета меняем
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
+                }
+            }
+        }
+
+        public void Render(Graphics g)
+        {
+            // ну тут так и быть уж сам впишу...
+            // это то же самое что на форме в методе Render
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+    }
+}
--- Chastitcy6/Form1.cs
+++ Chastitcy6/Form1.cs
@@ -12,8 +12,8 @@ namespace Chastitcy6
 {
     public partial class Form1 : Form
     {
-        // собственно список, пока пустой
-        List<Particle> particles = new List<Particle>();
+       
+        Emitter emitter = new Emitter(); // добавили эмиттер
 
         public Form1()
         {
@@ -22,104 +22,35 @@ namespace Chastitcy6
             // привязал изображение
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
 
-            /* УБИРАЮ ЭТОТ КОД
-         for (var i=0; i< 500; ++i)
-         {
-             var particle = new Particle();
-             particle.X = picDisplay.Image.Width / 2;
-             particle.Y = picDisplay.Image.Height / 2;
-             particles.Add(particle);
-         }
-         */
+           
 
 
 
         }
         // добавил функцию обновления состояния системы
-        private void UpdateState()
-        {
-            foreach (var particle in particles)
-            {
-                particle.Life -= 1; // уменьшаю здоровье
-                                    // если здоровье кончилось
-                if (particle.Life < 0)
-                {
-                    // восстанавливаю здоровье
-                    particle.Life = 20 + Particle.rand.Next(100);
-                    // новое начальное расположение частицы — это то, куда указывает курсор
-                    particle.X = MousePositionX;
+        
 
-                    /* ЭТО ДОБАВЛЯЮ, тут сброс состояния частицы */
-                    var direction = (double)Particle.rand.Next(360);
-                    var speed = 1 + Particle.rand.Next(10);
-
-                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
-                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
-                    /* конец ЭТО ДОБАВЛЯЮ  */
-
-                    particle.Radius = 2 + Particle.rand.Next(10);
-                }
-                else
-                {
-                    particle.X += particle.SpeedX;
-                    particle.Y += particle.SpeedY;
-                }
-            }
-
-            // добавил генерацию частиц
-            // генерирую не более 10 штук за тик
-            for (var i = 0; i < 10; ++i)
-            {
-                if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
-                {
-                    // а у тут уже наш новый класс используем
-                    var particle = new ParticleColorful();
-                    // ну и цвета меняем
-                    particle.FromColor = Color.Yellow;
-                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-                    particles.Add(particle);
-                }
-                else
-                {
-                    break; // а если частиц уже 500 штук, то ничего не генерирую
-                }
-            }
-        }
-
-        // функция рендеринга
-        private void Render(Graphics g)
-        {
-            // утащили сюда отрисовку частиц
-            foreach (var particle in particles)
-            {
-                particle.Draw(g);
-            }
-        }
-        // int counter = 0; эту строчку убрал
-
+        
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState(); // каждый тик обновляем систему
+            emitter.UpdateState(); // тут теперь обновляем эмиттер
+
 
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
                 g.Clear(Color.Black); // А ЕЩЕ ЧЕРНЫЙ ФОН СДЕЛАЮ
-                Render(g);
+                emitter.Render(g); // а тут теперь рендерим через эмиттер
             }
 
             picDisplay.Invalidate();
         }
 
-        // добавляем переменные для хранения положения мыши
-        private int MousePositionX = 0;
-        private int MousePositionY = 0;
+        
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            // в обработчике заносим положение мыши в переменные для хранения положения мыши
-            MousePositionX = e.X;
-            MousePositionY = e.Y;
+            // а тут в эмиттер передаем положение мыфки
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
